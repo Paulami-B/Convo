43950fc6d4bb3d5d5ecc205c022f79713ab0b28d
Paulami-B <paulamiibanerjee@gmail.com>
2025-03-09 11:21:41 +0530

    Icon added

---
+++ client/src/components/Icon.tsx
@@ -0,0 +1,15 @@
+interface IconProps{
+    size: string
+    text?: string
+    classProps?: string
+}
+
+export default function Icon({size, text, classProps}: IconProps) {
+  return (
+    <div className="h-auto pb-2 flex items-center">
+        <div className={`${classProps} font-matemasie ${size} leading-[1.5] bg-gradient-to-r from-yellow-400 via-amber-600 to-orange-600 dark:from-yellow-200 dark:via-amber-400 dark:to-orange-400 inline-block text-transparent bg-clip-text`}>
+            {text ? text : 'C'}
+        </div>
+    </div>
+  )
+}
